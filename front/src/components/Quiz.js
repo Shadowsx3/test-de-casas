6d8c1cec78cedfc483cfec7de8975591db7e5bc5
Alexia Feo <85040741+AzudurI@users.noreply.github.com>
2023-08-13 16:51:46 -0300

    updated quiz a lil bit
    now u can change questions & answers yay!

--- front/src/components/Quiz.js
+++ front/src/components/Quiz.js
@@ -3,6 +3,8 @@ import { questions } from "./Questions";
 
 export default function Quiz() {
   const answerValue = [0, 0, 0, 0];
+  const [currentQuestion, setCurrentQuestion] = useState(23);
+
   return (
     <>
       {false ? (
@@ -15,13 +17,16 @@ export default function Quiz() {
             <div className="Text">
               <span>Question 1</span>/{questions.length}
             </div>
-            <div className="Text">{questions[0].questionText}</div>
+            <div className="Text">
+              {questions[currentQuestion].questionText}
+            </div>
           </div>
           <div className="Text-Background">
-            <button>Answer 1</button>
-            <button>Answer 2</button>
-            <button>Answer 3</button>
-            <button>Answer 4</button>
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button>{answerOption.answerText}</button>
+              )
+            )}
           </div>
         </>
       )}
