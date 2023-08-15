b4c25535445684ab26d341da74acc7790794e163
Alexia Feo <85040741+AzudurI@users.noreply.github.com>
2023-08-15 14:01:22 -0300

    added stuff, added stuff in css
    stuff is a lil bit too big but dont even worry about it i'll fix it some other time

--- front/src/components/Quiz.js
+++ front/src/components/Quiz.js
@@ -2,29 +2,41 @@ import React, { useState } from "react";
 import { questions } from "./Questions";
 
 export default function Quiz() {
-  const answerValue = [0, 0, 0, 0];
-  const [currentQuestion, setCurrentQuestion] = useState(23);
+  const answerTotal = [0, 0, 0, 0];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleAnswerButtonClick = (answerOption) => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      alert("finfinfinfin");
+    }
+  };
 
   return (
     <>
+      <img className="Quiz-Logo" src="./TEST RECURSOS/1.png" />
       {false ? (
         <div className="Text-Background">
           You scored 1 out of {questions.length}
         </div>
       ) : (
         <>
-          <div className="Text-Background">
+          <div className="Question-Background">
             <div className="Text">
-              <span>Question 1</span>/{questions.length}
-            </div>
-            <div className="Text">
-              {questions[currentQuestion].questionText}
+              <span>Pregunta 1</span>/{questions.length}
+              <div className="Question-Text">
+                {questions[currentQuestion].questionText}
+              </div>
             </div>
           </div>
-          <div className="Text-Background">
+          <div className="Question-1">
             {questions[currentQuestion].answerOptions.map(
               (answerOption, index) => (
-                <button>{answerOption.answerText}</button>
+                <button onClick={() => handleAnswerButtonClick()}>
+                  {answerOption.answerText}
+                </button>
               )
             )}
           </div>
