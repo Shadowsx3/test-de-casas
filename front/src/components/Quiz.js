8fa407e3890f35e2fdefaba0e867f00893d1b8b6
Manuel Buslon (Bass) <manuelbuslon22@gmail.com>
2023-08-16 23:49:20 -0300

    Listooo

--- front/src/components/Quiz.js
+++ front/src/components/Quiz.js
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import { questions } from "./Questions";
 
 export default function Quiz() {
-  const answerTotal = [0, 0, 0, 0];
+  const [responses, setResponses] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const limit = 25;
 
-  const handleAnswerButtonClick = (answerOption) => {
+  const handleAnswerButtonClick = (index) => {
+    setResponses([...responses, [currentQuestion, index]]);
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
+    if (nextQuestion < limit) {
       setCurrentQuestion(nextQuestion);
     } else {
-      alert("finfinfinfin");
+      console.log(responses);
+      alert("No more questions for you");
     }
   };
 
@@ -25,16 +28,20 @@ export default function Quiz() {
         <>
           <div className="Question-Background">
             <div className="Text">
-              <span>Pregunta 1</span>/{questions.length}
+              <span>Pregunta {responses.length + 1}</span>/{questions.length}
               <div className="Question-Text">
                 {questions[currentQuestion].questionText}
               </div>
             </div>
           </div>
-          <div className="Question-1">
+          <div className="Questions">
             {questions[currentQuestion].answerOptions.map(
               (answerOption, index) => (
-                <button onClick={() => handleAnswerButtonClick()}>
+                <button
+                  className={`Question-${index + 1}`}
+                  key={index}
+                  onClick={() => handleAnswerButtonClick(index)}
+                >
                   {answerOption.answerText}
                 </button>
               )
